9c3821606031edf6ac0ea7d0e40d351b5457528e
Zain Mayoof <168248316+ZainMayoof@users.noreply.github.com>
2025-04-11 03:26:18 +0300

    Update script.js
    Set mission stage durations and initial timing

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 //Mission stages array
-//basic loop to iterate through the stages
+    //basic loop to iterate through the stages
+
+
+// Mission stage durations object
+    //Set durations for each stage
